f4386d01d21a003b116413f25fb52f4acb813a40
Khoi1201 <Acknowledge999@gmail.com>
2022-09-23 09:58:05 +0700

    change port

--- app.js
+++ app.js
@@ -153,6 +153,6 @@ if (port == null || port == "") {
   port = 3000;
 };
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server has started successfully");
 });
